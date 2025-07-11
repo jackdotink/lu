c019dd0ef70cdc8502a4d6fb2bc5f365abdee8f9
jack.ink <jackdotink@gmail.com>
2025-07-11 16:34:07 -0500

    maybe it will work with windows now

--- lu-sys/build.rs
+++ lu-sys/build.rs
@@ -1,18 +1,48 @@
 use cmake::Config;
 
 fn main() {
-    let dst = Config::new("luau")
+    let mut config = Config::new("luau");
+
+    config
         .define("LUAU_BUILD_CLI", "OFF")
         .define("LUAU_BUILD_TESTS", "OFF")
         .define("LUAU_BUILD_WEB", "OFF")
         .define("LUAU_EXTERN_C", "ON")
         .define("LUAU_STATIC_CRT", "ON")
+        .define("LUAU_ENABLE_ASSERT", "ON")
         .no_build_target(true)
-        .build();
+        .profile(if cfg!(debug_assertions) {
+            "RelWithDebInfo"
+        } else {
+            "Release"
+        });
 
+    #[cfg(target_os = "windows")]
+    config.cxxflag("/EHsc");
+
+    let dst = config.build();
+
+    #[cfg(target_os = "windows")]
+    {
+        #[cfg(debug_assertions)]
+        println!(
+            "cargo:rustc-link-search=native={}/build/RelWithDebInfo",
+            dst.display()
+        );
+        #[cfg(not(debug_assertions))]
+        println!(
+            "cargo:rustc-link-search=native={}/build/Release",
+            dst.display()
+        );
+    }
+
+    #[cfg(not(target_os = "windows"))]
     println!("cargo:rustc-link-search=native={}/build", dst.display());
+
     println!("cargo:rustc-link-lib=static=Luau.VM");
     println!("cargo:rustc-link-lib=static=Luau.Compiler");
     println!("cargo:rustc-link-lib=static=Luau.Ast");
+
+    #[cfg(not(target_os = "windows"))]
     println!("cargo:rustc-link-lib=stdc++");
 }
